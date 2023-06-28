45f109a3a2aecf89d4b488b18d0ac12fd003699b
NguyenNam1102 <namnguyenlllh@gmail.com>
2023-06-29 00:21:34 +0700

    Commit message

--- src/index.js
+++ src/index.js
@@ -2,11 +2,17 @@ const path = require('path')
 const express = require('express')
 const morgan = require('morgan')
 const handlebars = require('express-handlebars')
+const { log } = require('console')
 
 const app = express()
 const port = 3001
 
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(express.urlencoded({
+    extended: true
+}))
+app.use(express.json())
 
 // HTTP logger
 app.use(morgan('combined'))
@@ -21,7 +27,24 @@ app.get('/', (req, res) => {
 })
 
 app.get('/news', (req, res) => {
+    // console.log(req.params)
+
     res.render('news')
+})
+
+app.get('/search', (req, res) => {
+    // Query parameters (tham số truy vấn)
+    // http://localhost:3001/search?q=namnguyen&age=23&add=TN
+    // console.log(req.query)
+    // const { add } = req.query
+    // console.log(add)
+
+    res.render('search')
+})
+
+app.post('/search', (req, res) => {
+    console.log(req.body);
+    res.send('hello kitty')
 })
 
 app.listen(port, () => {
