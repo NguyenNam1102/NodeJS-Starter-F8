22d4d3cbb50472d162729578ec3ae96707e186bd
NguyenNam1102 <namnguyenlllh@gmail.com>
2023-07-08 00:16:08 +0700

    [MVC] Routes & Controllers

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan')
 const handlebars = require('express-handlebars')
 const { log } = require('console')
 
+const route = require('./routers')
+
 const app = express()
 const port = 3001
 
@@ -22,30 +24,33 @@ app.engine('hbs', handlebars.engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'resources/views'))
 
-app.get('/', (req, res) => {
-    res.render('home')
-})
+// Routes init
+route(app)
 
-app.get('/news', (req, res) => {
-    // console.log(req.params)
+// app.get('/', (req, res) => {
+//     res.render('home')
+// })
 
-    res.render('news')
-})
+// app.get('/news', (req, res) => {
+//     // console.log(req.params)
 
-app.get('/search', (req, res) => {
-    // Query parameters (tham số truy vấn)
-    // http://localhost:3001/search?q=namnguyen&age=23&add=TN
-    // console.log(req.query)
-    // const { add } = req.query
-    // console.log(add)
+//     res.render('news')
+// })
 
-    res.render('search')
-})
+// app.get('/search', (req, res) => {
+//     // Query parameters (tham số truy vấn)
+//     // http://localhost:3001/search?q=namnguyen&age=23&add=TN
+//     // console.log(req.query)
+//     // const { add } = req.query
+//     // console.log(add)
 
-app.post('/search', (req, res) => {
-    console.log(req.body);
-    res.send('hello kitty')
-})
+//     res.render('search')
+// })
+
+// app.post('/search', (req, res) => {
+//     console.log(req.body);
+//     res.send('hello kitty')
+// })
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
---
+++ src/routers/index.js
@@ -0,0 +1,36 @@
+const newsRouter = require('./news')
+const siteRouter = require('./site')
+
+function route(app) {
+    // app.get('/', (req, res) => {
+    //     res.render('home')
+    // })
+
+    // app.get('/news', (req, res) => {
+    //     // console.log(req.params)
+
+    //     res.render('news')
+    // })
+
+    app.use('/news', newsRouter)
+
+    app.use('/', siteRouter)
+
+
+    // app.get('/search', (req, res) => {
+    //     // Query parameters (tham số truy vấn)
+    //     // http://localhost:3001/search?q=namnguyen&age=23&add=TN
+    //     // console.log(req.query)
+    //     // const { add } = req.query
+    //     // console.log(add)
+
+    //     res.render('search')
+    // })
+
+    // app.post('/search', (req, res) => {
+    //     console.log(req.body);
+    //     res.send('hello kitty')
+    // })
+}
+
+module.exports = route
