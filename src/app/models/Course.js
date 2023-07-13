ea8e4c3639dd7e66372293ce5d4ff9b1fb4942bf
NguyenNam1102 <namnguyenlllh@gmail.com>
2023-07-14 00:14:03 +0700

    add

--- src/app/models/Course.js
+++ src/app/models/Course.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
+const slug = require('mongoose-slug-updater');
+
+mongoose.plugin(slug);
+
 const Schema = mongoose.Schema;
 
 const Course = new Schema({
     name: { type: String, maxLength: 255 },
     description: { type: String },
     image: { type: String },
-    createdAt: { type: Date, default: Date.now },
-    updatedAt: { type: Date, default: Date.now },
+    videoId: { type: String },
+    level: { type: String },
+    slug: { type: String, slug: "name", unique: true },
+}, {
+    timestamps: true,
 });
 
 module.exports = mongoose.model('Course', Course);
