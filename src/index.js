701d354822903b8ad2b0cbb2ff320b07979cc281
NguyenNam1102 <namnguyenlllh@gmail.com>
2023-07-15 00:28:30 +0700

    add

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const morgan = require('morgan');
+const methodOverride = require('method-override')
 const handlebars = require('express-handlebars');
 const { log } = require('console');
 
@@ -22,13 +23,21 @@ app.use(
 );
 app.use(express.json());
 
+app.use(methodOverride('_method'))
+
 // HTTP logger
 app.use(morgan('combined'));
 
 // Template engine
 app.engine(
     'hbs',
-    handlebars.engine({ defaultLayout: 'main', extname: '.hbs' }),
+    handlebars.engine({
+        defaultLayout: 'main',
+        extname: '.hbs',
+        helpers: {
+            sum: (a, b) => a + b,
+        }
+    }),
 );
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources', 'views'));
