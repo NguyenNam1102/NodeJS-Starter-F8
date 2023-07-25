332672c53cddef89eb51aed87d81e00d4872fae6
NguyenNam1102 <namnguyenlllh@gmail.com>
2023-07-25 23:55:11 +0700

    Sort middleware

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -5,7 +5,17 @@ class MeController {
 
     // [GET] /me/stored/courses
     storedCourses(req, res, next) {
-        Promise.all([Course.find({}), Course.findDeleted({})])
+
+        // res.json(res.locals._sort)
+        let courseQuery = Course.find({})
+
+        if (req.query.hasOwnProperty('_sort')) {
+            courseQuery = courseQuery.sort({
+                [req.query.column]: req.query.type
+            })
+        }
+
+        Promise.all([courseQuery, Course.findDeleted({})])
             .then(([courses, deletedCount]) =>
                 res.render('me/stored-courses', {
                     courses: multipleMongooseToObject(courses),
--- src/index.js
+++ src/index.js
@@ -8,6 +8,8 @@ const { log } = require('console');
 const route = require('./routers');
 const db = require('./config/db');
 
+const SortMiddleware = require('./app/middlewares/SortMiddleware')
+
 // Connect to DB
 db.connect()
 
@@ -24,6 +26,9 @@ app.use(
 app.use(express.json());
 
 app.use(methodOverride('_method'))
+
+// Custom middlewares
+app.use(SortMiddleware)
 
 // Test Middleware
 // app.get('/middleware',
@@ -54,6 +59,26 @@ app.engine(
         extname: '.hbs',
         helpers: {
             sum: (a, b) => a + b,
+            sortable: (field, sort) => {
+                const sortType = field === sort.column ? sort.type : 'default'
+                const icons = {
+                    default: 'oi oi-elevator',
+                    asc: 'oi oi-sort-ascending',
+                    desc: 'oi oi-sort-descending'
+                }
+                const types = {
+                    default: 'desc',
+                    asc: 'desc',
+                    desc: 'asc'
+                }
+
+                const icon = icons[sortType]
+                const type = types[sortType]
+
+                return `<a href="?_sort&column=${field}&type=${type}">
+                <span class="${icon}"></span>
+            </a>`
+            }
         }
     }),
 );
